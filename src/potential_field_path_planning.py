8cf546a2ae00d30476eff8cc7e1532f0c43bdb5c
EthanBK <bhou@ucdavis.edu>
2018-06-06 00:44:28 -0700

    working on PFPP

--- src/potential_field_path_planning.py
+++ src/potential_field_path_planning.py
@@ -1,9 +1,9 @@
 import math
+import numpy as np
 
 # Define parameters
-KP = 5.0    # Attrctive potential gain
-ETA = 100.0 # replusive potential gain
-
+KP = 5.0        # Attractive potential gain
+ETA = 100.0     # repulsive potential gain
 
 
 def cvt_coord_to_cube(index, dimension):
@@ -22,26 +22,36 @@ def cal_attractive_potential(x, y, z, end_point, dimension):
     return 0.5 * KP * math.sqrt((x - gx) ** 2 + (y - gy) ** 2 + (z - gz) ** 2)
 
 
-def cal_replusive_potnetial():
-    pass
+def cal_repulsive_potential(x, y, z, point_tree):
+    point = np.array([x, y, z])
+    distance = point_tree.query(point)
+
 
 
 def calculate_potential_field_map(nodes_list):
-    for k in range(len(nodes_list[0][0])):
+    for i in range(len(nodes_list)):
         for j in range(len(nodes_list[0])):
-            for i in range(len(nodes_list)):
+            for k in range(len(nodes_list[0][0])):
                 pass
 
     return nodes_list
 
 
+# Using KdTree to calculate the nearest distance from each point to obstacle
+def get_near_dis_map(nodes_lsit, point_tree):
+    # todo: how to get position info of each node in nodes_lists?
+    dis_map = []
+    return dis_map
+
+
 def potential_field_planning(nodes_list, dimension, boudnary, point_tree, start_index, end_index):
     # Define parameters
-    c_dis = 5
     path = []
+    from_node = []
+    dis_map = get_near_dis_map(nodes_list, point_tree)
     potential_map = calculate_potential_field_map(nodes_list)
 
-    return path
+    return end_index, from_node
 
 
 def main(nodes_list, dimension, boundary, start_end, point_tree):
